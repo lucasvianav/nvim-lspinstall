9a50c8c592f59342e3fa4bbdb85b27c2529da8ef
Lucas Viana <oviana@outlook.com>
2021-09-27 11:44:41 -0300

    Edit AngularLS config (issue #72)

--- lua/lspinstall/servers/angular.lua
+++ lua/lspinstall/servers/angular.lua
@@ -1,9 +1,23 @@
-local config = require"lspinstall/util".extract_config("angularls")
-config.default_config.cmd[1] = "./node_modules/.bin/ngserver"
+local config = require("lspinstall/util").extract_config("angularls")
 
-return vim.tbl_extend('error', config, {
-  install_script = [[
-  ! test -f package.json && npm init -y --scope=lspinstall || true
-  npm install @angular/language-server
-  ]]
+local cmd = {
+	"./node_modules/.bin/ngserver",
+	"--stdio",
+	"--tsProbeLocations",
+	"./node_modules",
+	"--ngProbeLocations",
+	"./node_modules",
+	"--experimental-ivy",
+}
+
+config.default_config.cmd = cmd
+config.default_config.on_new_config = function(new_config, new_root_dir)
+	new_config.cmd = cmd
+end
+
+return vim.tbl_extend("error", config, {
+	install_script = [[
+	! test -f package.json && npm init -y --scope=lspinstall || true
+	npm install @angular/language-server @angular/language-service typescript --save
+	]],
 })
